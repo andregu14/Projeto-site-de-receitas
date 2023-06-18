808b88eddb7993274330fed6ac6a910e3c24533d
André Gustavo <andregustavo147@gmail.com>
2023-06-18 20:44:18 -0300

    Create search-delete-button.js
    Adicionei funcionalidade para deletar o conteúdo da caixa de pesquisa usando um ícone custom.

---
+++ assets/scripts/search-delete-button.js
@@ -0,0 +1,23 @@
+const searchInput = document.querySelector('input[type="search"]')
+const searchIcon = document.getElementById('fa-xmark')
+
+if (searchInput.value === '') {
+    searchIcon.style.display = 'none'
+}
+
+searchInput.addEventListener('input', () => {
+
+    if (searchInput.value === '') {
+        searchIcon.style.display = 'none'
+    } else {
+        searchIcon.style.display = 'block'
+    }
+})
+
+function Delete() {
+
+    if (searchInput.value !== '') {
+        searchInput.value = ''
+        searchIcon.style.display = 'none'
+    }
+}
